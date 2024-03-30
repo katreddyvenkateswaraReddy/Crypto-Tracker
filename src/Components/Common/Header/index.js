8613d57839eedeba6cd77937a871c6264450a12f
katreddyvenkateswaraReddy <katreddy41@gmail.com>
2024-03-31 05:11:29 +0530

    newChanges

--- src/Components/Common/Header/index.js
+++ src/Components/Common/Header/index.js
@@ -1,28 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import AnchorTemporaryDrawer from "./drawer";
 import Button from "../Button";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import Switch from "@mui/material/Switch";
+import { toast } from "react-toastify";
 
-const Footer = () => {
-    return(
-        <div className="navbar">
-            <h1 className="logo">CryptoTracker<span style={{color: "var(--blue)"}}>.</span></h1>
-            <div className="navbar-links">
-                <Link to="/"><p className="link">Home</p></Link>
-                <Link to="/compare"><p className="link">Compare</p></Link>
-                <Link to="/watchlist"><p className="link">Watchlist</p></Link>
-                <Link to="/dashboard">
-                    <Button text={"Dashboard"} onClick={()=>{console.log("clicked")}}/>
-                </Link>
-                
-                {/* <Button text={"Share"} outlined={true} onClick={()=>{console.log("clicked")}}/> */}
-            </div>
-            <div className="mobile-drawer">
-                <AnchorTemporaryDrawer />
-            </div>
-        </div>
-    )
-}
+const Header = () => {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") == "dark" ? true : false
+  );
 
-export default Footer
+  useEffect(() => {
+    if (localStorage.getItem("theme") == "dark") {
+      setDark();
+    } else {
+      setLight();
+    }
+  }, []);
+
+  const changeMode = () => {
+    if (localStorage.getItem("theme") != "dark") {
+      setDark();
+    } else {
+      setLight();
+    }
+    setDarkMode(!darkMode);
+    toast.success("Theme Changed!");
+  };
+
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  };
+
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  };
+
+  return (
+    <div className="navbar">
+      <h1 className="logo">
+        CryptoTracker<span style={{ color: "var(--blue)" }}>.</span>
+      </h1>
+      <div className="navbar-links">
+        <Switch checked={darkMode} onClick={() => changeMode()} />
+        <Link to="/">
+          <p className="link">Home</p>
+        </Link>
+        <Link to="/compare">
+          <p className="link">Compare</p>
+        </Link>
+        <Link to="/watchlist">
+          <p className="link">Watchlist</p>
+        </Link>
+        <Link to="/dashboard">
+          <Button
+            text={"Dashboard"}
+            onClick={() => {
+              console.log("clicked");
+            }}
+          />
+        </Link>
+
+        {/* <Button text={"Share"} outlined={true} onClick={()=>{console.log("clicked")}}/> */}
+      </div>
+      <div className="mobile-drawer">
+        <AnchorTemporaryDrawer />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
